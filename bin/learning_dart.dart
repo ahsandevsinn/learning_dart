2b76be9ae06928a374d6fb215f111fcf0676b9cc
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-03-05 23:29:16 +0500

    Initial commit

---
+++ bin/learning_dart.dart
@@ -0,0 +1,50 @@
+
+import 'package:learning_dart/learning_dart.dart' as learning_dart;
+
+void main() {
+  // print('Hello world: ${learning_dart.calculate()}!'); this is a single line comment 
+
+  /**
+   This 
+   is 
+   a
+   MultiLine Comment
+   */
+
+///
+///this 
+///is a document comment
+///  
+    // int a = 10;
+    // double b = 2.0;
+    // String name = "Ahsan";
+    // bool isMale = true;
+    // print(a);
+    // print(b);
+    // print(name);
+    // print(isMale);
+
+    // dynamic name = "Ahsan";
+    // print(name);
+    // name = "Khan";
+    // print(name);
+
+
+    // final age = 28;
+    // print(age);
+    // final int userAge = 20;
+    // print(userAge);
+    // userAge = 39;
+    
+
+//  const age = 48;
+//  print(age);
+
+
+//  const int userAge  = 95;
+//  print(userAge);
+int a = 10;
+// a = NULL;
+print(a);
+
+}
