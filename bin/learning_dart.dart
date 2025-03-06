fc668c9ce318160de1e381fb5ee884104912780c
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-03-06 21:35:37 +0500

    What will be the value of y after executing the following code?

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,53 +1,8 @@
 
 
+import 'package:learning_dart/Operators/unary_postfix.dart';
+
 void main() {
-  // print('Hello world: ${learning_dart.calculate()}!'); this is a single line comment 
+  UnaryPostfix.postfix();
 
-  /**
-   This 
-   is 
-   a
-   MultiLine Comment
-   */
-
-///
-///this 
-///is a document comment
-///  
-    // int a = 10;
-    // double b = 2.0;
-    // String name = "Ahsan";
-    // bool isMale = true;
-    // print(a);
-    // print(b);
-    // print(name);
-    // print(isMale);
-
-    // dynamic name = "Ahsan";
-    // print(name);
-    // name = "Khan";
-    // print(name);
-
-
-    // final age = 28;
-    // print(age);
-    // final int userAge = 20;
-    // print(userAge);
-    // userAge = 39;
-    
-
-//  const age = 48;
-//  print(age);
-
-
-//  const int userAge  = 95;
-//  print(userAge);
-// int a = 10;
-// // a = NULL;
-// print(a);
-
-// Evaluate the following expression and determine the value of x after execution:
-// int x = 5;
-// x++;
-// print(x);
 }
---
+++ lib/Operators/unary_postfix.dart
@@ -0,0 +1,15 @@
+class UnaryPostfix {
+  static postfix(){
+
+  // Evaluate the following expression and determine the value of x after execution:
+// int x = 5;
+// x++;
+// print(x);
+
+// What will be the value of y after executing the following code?
+
+// int y = 10;
+// y--;
+// print(y);
+  }
+}
