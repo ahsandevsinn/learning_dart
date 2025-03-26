9c3e82dd171e77bd0237765d8561309ce90b133f
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-03-26 21:50:37 +0500

    data types

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -2,9 +2,11 @@
 
 import 'package:learning_dart/Comments/comment.dart';
 import 'package:learning_dart/Operators/unary_postfix.dart';
+import 'package:learning_dart/Variables/variables.dart';
 
 void main() {
   UnaryPostfix.postfix();
   Comment.comment();
+  Variables.variable();
 
 }
---
+++ lib/Variables/variables.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+
+class Variables {
+  static variable (){
+    // int gfg = 10;
+    // double gfg2 = 2.7;
+    // bool gfg3 = false;
+    // String gfg4 = "0";
+    // String gfg5 = "GFG";
+    // print(gfg);
+    // print(gfg2);
+    // print(gfg3);
+    // print(gfg4);
+    // print(gfg5);
+
+  }
+}
