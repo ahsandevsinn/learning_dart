d09262a1fd4cef8faf28ed963aedc7a3066faec8
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-03-26 21:10:25 +0500

    Single Line Comment

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,8 +1,10 @@
 
 
+import 'package:learning_dart/Comments/comment.dart';
 import 'package:learning_dart/Operators/unary_postfix.dart';
 
 void main() {
   UnaryPostfix.postfix();
+  Comment.comment();
 
 }
---
+++ lib/Comments/comment.dart
@@ -0,0 +1,9 @@
+class Comment {
+  // There are three types of comments 1 single line comment 2 multi-line comment 3 documentation comment 
+  // This is a single line comment 
+
+  static comment (){
+    double area = 3.14 * 4 * 4;
+    print(area); 
+  }
+}
