a82641c2e1a13eed029a6a5bfbcfb5d37bf010d0
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-04-08 08:24:03 +0500

    variables and datatypes

--- lib/Variables/variables.dart
+++ lib/Variables/variables.dart
@@ -2,39 +2,42 @@ import 'dart:ffi';
 
 class Variables {
   static variable (){
-    // int gfg = 10;
-    // double gfg2 = 2.7;
+    // int gfg1 = 10;
+    // double gfg2 = 0.2;
     // bool gfg3 = false;
     // String gfg4 = "0";
-    // String gfg5 = "GFG";
-    // print(gfg);
+    // String gfg5 = "Geeks for Geeks";
+    // print(gfg1);
     // print(gfg2);
     // print(gfg3);
     // print(gfg4);
     // print(gfg5);
 
-    // dynamic geek = "Geeks for Geeks";
+    // var geek = 'Geeks For Geeks';
     // print(geek);
-    // geek = 2.6;
+    // geek = "hello";  var datatype ki sath sirf same datatype ka data declare kar saktay hain same variable may multiple data types nai store karsaktay
+    // print(geek);
+    // geek = "Ahsan";
     // print(geek);
 
+     // dynamic geek = 'Geeks For Geeks';
+    // print(geek);
+    // geek = "hello";  dynamic data type ki variable may hum kisi bhi data type ka data store kar saktay hain 
+    // print(geek);
+    // geek = "Ahsan";
+    // print(geek);
 
-    // final geek1 = "Geeks for Geeks";
-    // print(geek1);
-    // final String geek2 = "Geeks for Geeks";
-    // // final geek1 = "Ahsan";
-    // print(geek2);
-
+    // final String geek ;
+    // geek = "Geeks for Geeks";
+    // print(geek);
 
     // const geek = "Geeks for Geeks";
     // print(geek);
-    // const String a = "Geeks for Geeks";
-    // // geek = "jfjd";
-    // print(a);
 
-// int? a;
-// a=null;
-// print(a);
+    // final dataype ko hum value ki without bhi declare karsaktay hain jabki const ko value required hoti hai jab hum declare kartay hain 
+
+    
+    
 
   }
 }
