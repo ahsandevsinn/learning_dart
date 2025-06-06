768cf4642c4d179e21b581179521dc3d89aecc6b
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-06-06 20:14:42 +0500

    variable

--- lib/Variables/variables.dart
+++ lib/Variables/variables.dart
@@ -1,19 +1,25 @@
 
 class Variables {
   static variable (){
-   var name = "Ahsan";
-   var age = 19;
-   var height = 4.2;
-   var favouriteColors = ["Green", "Red", "Blue"];
-   var person = {
-    "name" : "Ahsan",
-    "age" : "age",
-   };
-   print(name);
-   print(age);
-   print(height);
-   print(favouriteColors);
-   print(person);
+  //  var name = "Ahsan";
+  //  var age = 19;
+  //  var height = 4.2;
+  //  var favouriteColors = ["Green", "Red", "Blue"];
+  //  var person = {
+  //   "name" : "Ahsan",
+  //   "age" : "age",
+  //  };
+  //  print(name);
+  //  print(age);
+  //  print(height);
+  //  print(favouriteColors);
+  //  print(person);
+
+  String name = "Ahsan";
+  print(name);
+
+  var userName , age , location;
+
 
   }
 }
