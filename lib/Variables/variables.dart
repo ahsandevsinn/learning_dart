44420ab7abf7a99e08366ca01974d2161f1a1e29
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-06-13 09:08:16 +0500

    Can a local variable have a default value without initialization?

--- lib/Variables/variables.dart
+++ lib/Variables/variables.dart
@@ -30,14 +30,31 @@ class Variables {
     // What is the default value of an uninitialized int in a class in Dart?
   
 
-  var a =  MyValue();
-  print(a.a);
-  // value is null
+  // var a =  MyValue();
+  // print(a.a);
+  // // value is null
+
+  // // Can a local variable have a default value without initialization? score 4.5/5
+  // var a;
+  // print(a);
+  // dynamic b ;
+  // print(b);
+  // int? c;
+  // print(c);
+  // bool? d;
+  // print(d);
+  // String? e;
+  // print(e);
+  // final  f;
+  // print(f);
+  // const g ; 
+  // print(g);
+  // // yes a local like var , dynamic , variable without initialization default value is null and int , bool , string must be nullable otherwise its show varvale must be initailize also const and final need must initialize value
   }
 }
 
 
-class MyValue {
-  int? a;
+// class MyValue {
+//   int? a;
   
-} 
+// } 
