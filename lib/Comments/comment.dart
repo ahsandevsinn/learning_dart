0c88ed8bf8eaea457ed40ae6102499ee5b47c52a
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-16 02:09:11 +0500

    comments

--- lib/Comments/comment.dart
+++ lib/Comments/comment.dart
@@ -1,24 +1,9 @@
-class Comment {
-  // There are three types of comments 1 single line comment 2 multi-line comment 3 documentation comment 
-  // This is a single line comment 
-
-  static comment (){
-    // double area = 3.14 * 4 * 4;
-    //  // It prints the area
-    // // of a circle of radius = 4 
-    // print(area); 
-
-  /*
-   These are 
-   multiple line 
-   of comments 
-   */
+// Single Line Comment
 
 
-  /// THis is a documented comments
+/* This is 
+a multi line comment
+*/
 
-
-
-
-  }
-}
+/// This is a documentation 
+/// comments
