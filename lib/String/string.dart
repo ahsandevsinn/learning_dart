2e8c341fdf168bcd002973ed4fcf845ea2fd6a56
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-19 00:00:17 +0500

    practice subrting question 1

--- lib/String/string.dart
+++ lib/String/string.dart
@@ -1,19 +1,20 @@
 class StringClasss {
   static string(){
     // string interpolation ka matlab aik string ki ander variable ko add karna isko string interpolation boltay hain
-    String name = "Ahsan";
-    int age = 19;
-    String user = "My name is $name and my age is $age years old";
-    print(user.length);
-    print(user.contains("is"));
-    print(user.substring(0,10));
-    print(user.split(" "));
-    print(user.toLowerCase());
-    print(user.toUpperCase());
-    print(user.trim());
+    // String name = "Ahsan";
+    // int age = 19;
+    // String user = "My name is $name and my age is $age years old";
+    // print(user.length);
+    // print(user.contains("is"));
+    // print(user.substring(0,10));
+    // print(user.split(" "));
+    // print(user.toLowerCase());
+    // print(user.toUpperCase());
+    // print(user.trim());
 
     // common methods
-    
+    String name = "Flutter Developer";
+    print(name.substring(0,5));
     
   }
 }
