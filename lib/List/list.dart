505a1f245df34379f7dfd69a58f3dc79adc0e2b6
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-20 22:31:40 +0500

    Add List

--- lib/List/list.dart
+++ lib/List/list.dart
@@ -1,9 +1,24 @@
 class ListClass {
   static list(){
-    List<String> names = ["Ahsan","Khan","Usman"];
-    print(names.length);
-    print(names[0]);
-    print(names[1]);
-    print(names[2]);
+    // List<String> names = ["Ahsan","Khan","Usman"];
+    // print(names.length);
+    // print(names[0]);
+    // print(names[1]);
+    // print(names[2]);
+
+
+    // 🔹 2. Modify List (Add, Remove, Insert)
+    List<String> name = ["Hello Khan"];
+ name.add("Hello Ahsan");
+    print(name);
+    name.addAll(["AK,KK,RK"]);
+    print(name);
+    List<int> ages = [2,5,6,8];
+    ages.add(232);
+    print(ages); 
+    ages.addAll([20,34,56]);
+    print(ages);
+
+
   }
 }
