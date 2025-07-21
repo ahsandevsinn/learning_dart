69e6937d634a18ccd58521ebd17b4f5b50f6956c
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-21 23:55:03 +0500

    set with loops

--- lib/Map/map.dart
+++ lib/Map/map.dart
@@ -3,5 +3,17 @@ class MapClass {
   // a set is a  collection of unique items it removes duplicate data
   Set<String> names = {"Ahsan","Ali","Usman","Ahsan"};
   print(names);
+
+  for (var i = 0; i < names.length; i++) {
+   print("names $names"); 
+  }
+
+  for (var name in names) {
+    print(name);
+  }
+
+  names.forEach((name){
+    print("name $name");
+  });
  }
 }
