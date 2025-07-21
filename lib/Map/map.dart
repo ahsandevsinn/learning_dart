6e989889a00705939b109150b75240459141571b
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-22 01:13:35 +0500

    loop with map

--- lib/Map/map.dart
+++ lib/Map/map.dart
@@ -1,19 +1,35 @@
 class MapClass {
  static map(){
   // a set is a  collection of unique items it removes duplicate data
-  Set<String> names = {"Ahsan","Ali","Usman","Ahsan"};
-  print(names);
+  // Set<String> names = {"Ahsan","Ali","Usman","Ahsan"};
+  // print(names);
 
-  for (var i = 0; i < names.length; i++) {
-   print("names $names"); 
-  }
+  // for (var i = 0; i < names.length; i++) {
+  //  print("names $names"); 
+  // }
 
-  for (var name in names) {
-    print(name);
-  }
+  // for (var name in names) {
+  //   print(name);
+  // }
 
-  names.forEach((name){
-    print("name $name");
-  });
+  // names.forEach((name){
+  //   print("name $name");
+  // });
+
+Map<String,String> person = {
+  "name" : "Ahsan",
+  "Age" : "19",
+  "height": "5.0"
+};
+
+print(person['name']);
+
+for (var per in person.keys) {
+  print("per $per");
+}
+
+person.forEach((key,value){
+  print("key $key , value $value");
+});
  }
 }
