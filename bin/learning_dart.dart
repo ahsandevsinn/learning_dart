905b887a2265ece0874233d3e00dc61821320888
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-23 01:27:21 +0500

    logical operator

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -4,9 +4,10 @@ import 'package:learning_dart/List/list.dart';
 import 'package:learning_dart/Map/map.dart';
 import 'package:learning_dart/Operators/arithmetic_operator.dart';
 import 'package:learning_dart/Operators/assignment_operators.dart';
+import 'package:learning_dart/Operators/logical_opertors.dart';
 import 'package:learning_dart/Operators/relational_operators.dart';
 
 
 void main(){
- RelationalOperators.operators();
+LogicalOpertors.operators();
 }
---
+++ lib/Operators/logical_opertors.dart
@@ -0,0 +1,11 @@
+class LogicalOpertors {
+  static operators(){
+    bool a = true;
+    bool b = false;
+    
+    print(a&&b);
+    print(a||b);
+    print(!a);
+
+  }
+}
