250785225a904a03995efbe4932091c35a693b26
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-23 01:21:20 +0500

    Comparison Operators

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -4,8 +4,9 @@ import 'package:learning_dart/List/list.dart';
 import 'package:learning_dart/Map/map.dart';
 import 'package:learning_dart/Operators/arithmetic_operator.dart';
 import 'package:learning_dart/Operators/assignment_operators.dart';
+import 'package:learning_dart/Operators/relational_operators.dart';
 
 
 void main(){
- AssignmentOperators.operators();
+ RelationalOperators.operators();
 }
--- lib/Operators/relational_operators.dart
+++ lib/Operators/relational_operators.dart
@@ -5,24 +5,31 @@ class RelationalOperators {
   int a = 2;
   int b = 3;
 
-  var c = a> b;
-  print("a is greater than b $c");
+  // var c = a> b;
+  // print("a is greater than b $c");
 
-  var d = a < b;
-  print("a is less than b $d");
+  // var d = a < b;
+  // print("a is less than b $d");
 
-  var e = a >= b;
-  print("a is greater than equal to $e");
+  // var e = a >= b;
+  // print("a is greater than equal to $e");
 
-  var f = a <= b;
-  print("a is less than equal to $f");
+  // var f = a <= b;
+  // print("a is less than equal to $f");
 
-  var g = a == b;
-  print("a is equal to $g");
+  // var g = a == b;
+  // print("a is equal to $g");
 
-  var h = a != b;
-  print("a is equal to $h");
+  // var h = a != b;
+  // print("a is equal to $h");
 
+
+  print(a>b);
+  print(a<b);
+  print(a==b);
+  print(a!=b);
+  print(a>=b);
+  print(a<=b);
 
 
   }
