65738b202de0037a9957e6f48704184a89c7301e
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-23 02:59:11 +0500

    loops

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -2,6 +2,7 @@
 
 import 'package:learning_dart/CondtionalStatements/conditional_statement.dart';
 import 'package:learning_dart/List/list.dart';
+import 'package:learning_dart/Loop/loop.dart';
 import 'package:learning_dart/Map/map.dart';
 import 'package:learning_dart/Operators/arithmetic_operator.dart';
 import 'package:learning_dart/Operators/assignment_operators.dart';
@@ -11,5 +12,5 @@ import 'package:learning_dart/Operators/relational_operators.dart';
 
 
 void main(){
-  ConditionalStatement.operators();
+ Loop.loop();
 }
---
+++ lib/Loop/loop.dart
@@ -0,0 +1,30 @@
+class Loop {
+  static loop(){
+    // int a = 10;
+    // for (int i = 0; i < 10; i++){
+    // print("count $i");
+    // }
+
+    // int i = 0;
+    // while (i < 3){
+    //   print("count $i");
+    //   i++;
+    // }
+// int i = 0;
+// do {
+//   print("do while count $i");
+//   i++;
+// } while (i < 3);
+
+List<String> names = ["Ahsan","Khan", "Ali"];
+for(var name in names){
+  print("name $name")
+;  
+}
+
+
+names.forEach((name){
+  print("count $name");
+});
+  }
+}
