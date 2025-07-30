b0482945efacf6ad47dc99f06e8ac6c66c1ae822
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-30 23:29:55 +0500

    .where

--- lib/Collections/collections.dart
+++ lib/Collections/collections.dart
@@ -1,8 +1,9 @@
 class CollectionsClass {
 
   static listFunction(){
-    List<int> numbers = [1,2,3];
-    List<int> newNumbers = numbers.map((e)=> e*2).toList();
+    List<int> numbers = [1,2,3,6,8,44,5,2,7];
+    // List<int> newNumbers = numbers.map((e)=> e*2).toList();
+    List<int> newNumbers = numbers.where((e)=> e.isEven).toList();
     print(newNumbers);
   }  
 
