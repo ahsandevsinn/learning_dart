9b6e3a6061f6e5e763eaa43dfc59bd4677f5ffe0
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-08-08 23:45:54 +0500

    null check

--- lib/Variables/variables.dart
+++ lib/Variables/variables.dart
@@ -1,5 +1,5 @@
 class Variables {
-static late String userName;
+  static late String userName;
   static void variables() {
     //  there are three types of variables
     // 1 Static Variables
@@ -42,46 +42,46 @@ static late String userName;
 
     // Note dynamic variable store each data type data on same variable  but var only accept one data type value
 
-    // final and const variable 
-  //   final user = "Ahsna";
-  //   // final variable may value sirf aik bar set hoti hai or change nai hoti ye run time const hai iska matlab value program run per check hogi
+    // final and const variable
+    //   final user = "Ahsna";
+    //   // final variable may value sirf aik bar set hoti hai or change nai hoti ye run time const hai iska matlab value program run per check hogi
 
-  //   const user1 = "Ahsan";
-  //   // user1 = 1;
-  //   // const compile time const hai ye program run honay say phalay hi varialbe ki value check karta hai or change nai hota 
-   
-  // //  late variable 
-  // late String userName;
-  // userName = "Ahsan";
-  // print(userName);
+    //   const user1 = "Ahsan";
+    //   // user1 = 1;
+    //   // const compile time const hai ye program run honay say phalay hi varialbe ki value check karta hai or change nai hota
 
-  // late variable is liye use kartay hain ki may variable ki value bad may dounga abhi empty hai isliye may phalay late say variable declare kardo ga phir isko use karnay say phalay initiallize karo ga other wise error throw hoga
-  
-      // ┌────────────┐
-      // │            │
-      // │  const     │
-      // │            │
-      // └────┬───────┘
-      //      │
-      // Compile Time Value
-      // No Change Ever
-      // Optimized Memory
+    // //  late variable
+    // late String userName;
+    // userName = "Ahsan";
+    // print(userName);
 
-      // ┌────────────┐
-      // │            │
-      // │  final     │
-      // │            │
-      // └────┬───────┘
-      //      │
-      // Runtime Value
-      // No Change After Assign
-      // Flexible with logic
-   
-  // var name = "Ahsan";
-  // // var name may "Ahsan" String hai jab may var may koi value assign kartay hain to value ki base per uski data type define hoti ismay "Ahsan" aik string hai to var ki data type bhi String hai or ismay sirf dobara jab vale ko assign karna hoga same variable may to string value hi assign hogi
-  // final lastname = "Khan";
-  // lastname = ""
-  // final may hum same variable may value dobara assign ni karsaktay jab wo first time set hoti hai
+    // late variable is liye use kartay hain ki may variable ki value bad may dounga abhi empty hai isliye may phalay late say variable declare kardo ga phir isko use karnay say phalay initiallize karo ga other wise error throw hoga
+
+    // ┌────────────┐
+    // │            │
+    // │  const     │
+    // │            │
+    // └────┬───────┘
+    //      │
+    // Compile Time Value
+    // No Change Ever
+    // Optimized Memory
+
+    // ┌────────────┐
+    // │            │
+    // │  final     │
+    // │            │
+    // └────┬───────┘
+    //      │
+    // Runtime Value
+    // No Change After Assign
+    // Flexible with logic
+
+    // var name = "Ahsan";
+    // // var name may "Ahsan" String hai jab may var may koi value assign kartay hain to value ki base per uski data type define hoti ismay "Ahsan" aik string hai to var ki data type bhi String hai or ismay sirf dobara jab vale ko assign karna hoga same variable may to string value hi assign hogi
+    // final lastname = "Khan";
+    // lastname = ""
+    // final may hum same variable may value dobara assign ni karsaktay jab wo first time set hoti hai
 
 // const age = 29;
 // //  const may value fixed rehti hai jasisy constant hota
@@ -91,28 +91,32 @@ static late String userName;
 //   userName = "Ahsan";
 //   print(userName);
 
-  // variable scope 
-  //  void greet(){
-  //   var name1 = "Ahsan";
-  //  }
-  // function scope variable function may rehta hai
-  //  String name = "Ali";
-   
-  //  print(name);
+    // variable scope
+    //  void greet(){
+    //   var name1 = "Ahsan";
+    //  }
+    // function scope variable function may rehta hai
+    //  String name = "Ali";
 
-  // String? email;
-  // email = null;
+    //  print(name);
 
-  // print(email);
-  display("dsds");
+    // String? email;
+    // email = null;
 
+    // print(email);
+    // display("dsds");
+    String? name;
+    if (name != null) {
+      name = "Ajsan";
+    }
+    print(name);
   }
 }
 
-display(String? name){
- if (name != null) {
-   print("Welcome $name");
- }else{
-  print("Name not found");
- }
-}
+// display(String? name){
+//  if (name != null) {
+//    print("Welcome $name");
+//  }else{
+//   print("Name not found");
+//  }
+// }
